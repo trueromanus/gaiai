cb4709e8375f478e7b7a6f1c3c9481a7becda336
Roman Vladimirov <trueromanus@gmail.com>
2025-01-16 21:42:16 +0300

    + Added citizen

---
+++ src/Models/gamecitizen.cpp
@@ -0,0 +1,38 @@
+#include "gamecitizen.h"
+
+GameCitizen::GameCitizen(QObject *parent)
+    : QObject{parent}
+{}
+
+void GameCitizen::setTitle(const QString &title) noexcept
+{
+    if (m_title == title) return;
+
+    m_title = title;
+    emit titleChanged();
+}
+
+void GameCitizen::setStressLevel(int stressLevel) noexcept
+{
+    if (m_stressLevel == stressLevel) return;
+
+    m_stressLevel = stressLevel;
+    emit stressLevelChanged();
+    emit isMadChanged();
+}
+
+void GameCitizen::setLocation(const QString &location) noexcept
+{
+    if (m_location == location) return;
+
+    m_location = location;
+    emit locationChanged();
+}
+
+void GameCitizen::setOriginalLocation(const QString &originalLocation) noexcept
+{
+    if (m_originalLocation == originalLocation) return;
+
+    m_originalLocation = originalLocation;
+    emit originalLocationChanged();
+}
---
+++ src/Models/gamecitizen.h
@@ -0,0 +1,55 @@
+#ifndef GAMECITIZEN_H
+#define GAMECITIZEN_H
+
+#include <QObject>
+
+class GameCitizen : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString title READ title WRITE setTitle NOTIFY titleChanged FINAL)
+    Q_PROPERTY(bool isALive READ isALive NOTIFY isALiveChanged FINAL)
+    Q_PROPERTY(bool isNotALive READ isNotALive NOTIFY isNotALiveChanged FINAL)
+    Q_PROPERTY(bool isMad READ isMad NOTIFY isMadChanged FINAL)
+    Q_PROPERTY(int stressLevel READ stressLevel WRITE setStressLevel NOTIFY stressLevelChanged FINAL)
+    Q_PROPERTY(QString location READ location WRITE setLocation NOTIFY locationChanged FINAL)
+    Q_PROPERTY(QString originalLocation READ originalLocation WRITE setOriginalLocation NOTIFY originalLocationChanged FINAL)
+
+private:
+    QString m_title { "" };
+    bool m_isALive { true };
+    int m_stressLevel { 0 };
+    QString m_location { "" };
+    QString m_originalLocation { "" };
+    QList<QString> m_relatedTrafficLights { QList<QString>() };
+
+public:
+    explicit GameCitizen(QObject *parent = nullptr);
+
+    QString title() const noexcept { return m_title; }
+    void setTitle(const QString& title) noexcept;
+
+    bool isALive() const noexcept { return m_isALive; }
+    bool isNotALive() const noexcept { return !m_isALive; }
+    bool isMad() const noexcept { return m_stressLevel >= 2; }
+
+    int stressLevel() const noexcept { return m_stressLevel; }
+    void setStressLevel(int stressLevel) noexcept;
+
+    QString location() const { return m_location; }
+    void setLocation(const QString& location) noexcept;
+
+    QString originalLocation() const { return m_originalLocation; }
+    void setOriginalLocation(const QString& originalLocation) noexcept;
+
+signals:
+    void titleChanged();
+    void isALiveChanged();
+    void isNotALiveChanged();
+    void isMadChanged();
+    void stressLevelChanged();
+    void locationChanged();
+    void originalLocationChanged();
+
+};
+
+#endif // GAMECITIZEN_H
