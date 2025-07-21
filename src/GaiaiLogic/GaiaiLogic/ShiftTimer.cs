5dad079987fcd59b894208b08ede6a2e78de20cf
Roman Vladimirov <trueromanus@gmail.com>
2025-07-21 19:51:24 +0300

    + Added scaffold for project

---
+++ src/GaiaiLogic/GaiaiLogic/ShiftTimer.cs
@@ -0,0 +1,43 @@
+﻿namespace GaiaiLogic {
+
+    /// <summary>
+    /// Shift timer.
+    /// </summary>
+    internal class ShiftTimer {
+
+        private readonly TimeSpan m_startTime = new TimeSpan ( 21, 0, 0 );
+
+        private TimeSpan m_currentTime;
+
+        public ShiftTimer () {
+            m_currentTime = m_startTime;
+        }
+
+        /// <summary>
+        /// Current time.
+        /// </summary>
+        public TimeSpan CurrentTime => m_currentTime;
+
+        /// <summary>
+        /// Reset time to start of shift.
+        /// </summary>
+        public void Reset () {
+            m_currentTime = m_startTime;
+        }
+
+        /// <summary>
+        /// Run iteration and make step in time.
+        /// </summary>
+        public void RunIteration () {
+            // if we have five minutes before midnight
+            // we need to change time to make zero timespan because in other case it add to days TimeSpan which is not expected result
+            if ( m_currentTime == new TimeSpan ( 23, 55, 0 ) ) {
+                m_currentTime = TimeSpan.Zero;
+                return;
+            }
+            m_currentTime += TimeSpan.FromMinutes ( 5 );
+        }
+
+    }
+
+}
---
+++ src/GaiaiLogic/GasiaiLogicTests/ShiftTimerUnitTests.cs
@@ -0,0 +1,54 @@
+﻿using GaiaiLogic;
+
+namespace GaiaiLogicTests {
+
+    public class ShiftTimerUnitTests {
+
+        [Fact]
+        public void Reset_Completed_WithoutChangedTime () {
+
+            // arrange
+            var shiftTimer = new ShiftTimer ();
+
+            // act
+            shiftTimer.Reset ();
+
+            //assert
+            Assert.Equal ( new TimeSpan ( 21, 0, 0 ), shiftTimer.CurrentTime );
+        }
+
+        [Fact]
+        public void Reset_Completed_WithChangedTime () {
+
+            // arrange
+            var shiftTimer = new ShiftTimer ();
+            shiftTimer.RunIteration ();
+            shiftTimer.RunIteration ();
+            shiftTimer.RunIteration ();
+
+            // act
+            shiftTimer.Reset ();
+
+            //assert
+            Assert.Equal ( new TimeSpan ( 21, 0, 0 ), shiftTimer.CurrentTime );
+        }
+
+        [Fact]
+        public void RunIteration_Completed_TransitionOnNextDay () {
+
+            // arrange
+            var shiftTimer = new ShiftTimer ();
+            for ( var i = 0; i < 35; i++ ) {
+                shiftTimer.RunIteration ();
+            }
+
+            // act
+            shiftTimer.RunIteration ();
+
+            //assert
+            Assert.Equal ( TimeSpan.Zero, shiftTimer.CurrentTime );
+        }
+
+    }
+
+}
