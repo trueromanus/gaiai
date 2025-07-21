295c5c3b9672687e76d2556a195ea5256426097e
Roman Vladimirov <trueromanus@gmail.com>
2025-07-21 20:07:53 +0300

    + Added TriggersHub

---
+++ src/GaiaiLogic/GaiaiLogic/TriggersHub.cs
@@ -0,0 +1,26 @@
+﻿namespace GaiaiLogic {
+
+    /// <summary>
+    /// Class which is store happened triggers.
+    /// </summary>
+    internal class TriggersHub {
+
+        public readonly HashSet<string> m_triggeredThings = [];
+
+        public bool TriggerIsEnabled ( string trigger ) => m_triggeredThings.Contains ( trigger );
+
+        public void EnableTrigger ( string trigger ) {
+            if ( m_triggeredThings.Contains ( trigger ) ) return;
+
+            m_triggeredThings.Add ( trigger );
+        }
+
+        public void DisableTrigger ( string trigger ) {
+            if ( !m_triggeredThings.Contains ( trigger ) ) return;
+
+            m_triggeredThings.Remove ( trigger );
+        }
+
+    }
+
+}
---
+++ src/GaiaiLogic/GasiaiLogicTests/TriggerHubUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using GaiaiLogic;
+
+namespace GaiaiLogicTests {
+
+    public class TriggerHubUnitTests {
+
+        [Fact]
+        public void EnableTrigger_Completed_HappyPath () {
+            // arrange
+            var hub = new TriggersHub ();
+
+            // act
+            hub.EnableTrigger ( "test1" );
+
+            // assert
+            Assert.True ( hub.TriggerIsEnabled ( "test1" ) );
+        }
+
+        [Fact]
+        public void EnableTrigger_Completed_AlreadyAdded () {
+            // arrange
+            var hub = new TriggersHub ();
+            hub.EnableTrigger ( "test1" );
+
+            // act
+            hub.EnableTrigger ( "test1" );
+
+            // assert
+            Assert.True ( hub.TriggerIsEnabled ( "test1" ) );
+        }
+
+        [Fact]
+        public void DisableTrigger_Completed_HappyPath () {
+            // arrange
+            var hub = new TriggersHub ();
+            hub.EnableTrigger ( "test2" );
+
+            // act
+            hub.DisableTrigger ( "test2" );
+
+            // assert
+            Assert.False ( hub.TriggerIsEnabled ( "test2" ) );
+        }
+
+        [Fact]
+        public void DisableTrigger_Completed_NotExists () {
+            // arrange
+            var hub = new TriggersHub ();
+
+            // act
+            hub.DisableTrigger ( "test2" );
+
+            // assert
+            Assert.False ( hub.TriggerIsEnabled ( "test2" ) );
+        }
+
+    }
+
+}
