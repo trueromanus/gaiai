85d458c0f62abce764f0a90f84e5a199094f47a1
Roman Vladimirov <trueromanus@gmail.com>
2026-01-16 20:42:40 +0300

    Added scaffold for main menu

---
+++ src/GaiaiLogic/GaiaiLogic/Program.cs
@@ -0,0 +1,28 @@
+﻿using EmptyFlow.SciterAPI;
+using EmptyFlow.SciterAPI.Client.DeveloperConsole;
+
+namespace GaiaiLogic {
+
+    public static class Program {
+
+        [STAThread]
+        public static void Main() {
+            var host = new SciterAPIHost(Environment.CurrentDirectory);
+#if DEBUG
+            host.EnableDebugMode();
+#endif
+            host.EnableFeatures(SciterRuntimeFeatures.ALLOW_SOCKET_IO);
+            host.CreateWindow(asMain: true);
+#if DEBUG
+            host.LoadFile(@"C:\work\Repositories\HackingGame\game\gaiai\src\GaiaiLogic\GaiaiHTML\game.html");
+            //host.LoadFile(@"C:\work\Repositories\HackingGame\game\gaiai\src\GaiaiLogic\GaiaiHTML\stylew95.html");
+#endif
+            host.SetWindowCaption(host.MainWindow, "gaiai");
+            host.ShowWindow(host.MainWindow);
+            var console = new DeveloperConsole(host, host.MainWindow);
+            host.Process();
+        }
+
+    }
+
+}
