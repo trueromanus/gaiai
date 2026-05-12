4c1fc07319e4b6713c7e881417de9d423d162019
Roman Vladimirov <trueromanus@gmail.com>
2026-05-12 16:12:49 +0300

    Added scaffold for popup menu

---
+++ src/GaiaiLogic/GaiaiLogic/HTMLComponents/PopupMenu.cs
@@ -0,0 +1,45 @@
+﻿using EmptyFlow.SciterAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GaiaiLogic.HTMLComponents
+{
+
+    internal record PopupMenuItem(string Title, string Action, bool Enabled, bool Terminator);
+
+    internal class PopupMenu : SciterEventHandler
+    {
+
+        private readonly List<PopupMenuItem> m_items;
+
+        public PopupMenu(nint relatedThing, SciterAPIHost host) : base(relatedThing, host)
+        {
+            m_items = new List<PopupMenuItem>();
+
+            var childrens = Host.GetElementChildrens(relatedThing);
+            foreach (var child in childrens)
+            {
+                var action = Host.GetElementAttribute(child, "action");
+                var content = Host.GetElementHtml(child, false);
+                var terminator = Host.GetElementHasAttribute(child, "terminator");
+                var enabled = Host.GetElementHasAttribute(child, "enabled");
+                m_items.Add(new PopupMenuItem(content, action, enabled, terminator));
+            }
+
+        }
+
+        public void ClickOnMenu(ItemPopupMenu menu)
+        {
+
+        }
+
+        public void HoverOnMenu(ItemPopupMenu menu)
+        {
+
+        }
+
+        public override EventBehaviourGroups BeforeRegisterEvent() => EventBehaviourGroups.HANDLE_BEHAVIOR_EVENT;
+
+    }
+}
---
+++ src/GaiaiLogic/GaiaiLogic/HTMLComponents/PopupMenuItem.cs
@@ -0,0 +1,42 @@
+﻿using EmptyFlow.SciterAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GaiaiLogic.HTMLComponents
+{
+
+    internal class ItemPopupMenu : SciterEventHandler
+    {
+
+        private readonly PopupMenu m_menu;
+
+        public ItemPopupMenu(nint relatedThing, SciterAPIHost host) : base(relatedThing, host)
+        {
+            var parent = host.NodeParent(relatedThing);
+            m_menu = (host.GetEventHandlerByPointer(parent) as PopupMenu) ?? throw new Exception("Parent for ItemPopupMenu not PopupMenu");
+        }
+
+        public override EventBehaviourGroups BeforeRegisterEvent() => EventBehaviourGroups.HANDLE_MOUSE;
+
+        public override void MouseEvent(MouseEvents command, SciterPoint elementRelated, SciterPoint ViewRelated, KeyboardStates keyboardStates, DraggingType draggingMode, CursorType cursorType, nint target, nint dragging, bool isOnIcon, uint buttonState)
+        {
+            if (command == MouseEvents.MOUSE_CLICK)
+            {
+                m_menu.ClickOnMenu(this);
+            }
+
+            if (command == MouseEvents.MOUSE_ENTER)
+            {
+                m_menu.HoverOnMenu(this);
+            }
+
+            if (command == MouseEvents.MOUSE_LEAVE)
+            {
+
+            }
+        }
+
+    }
+
+}
